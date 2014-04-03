35c331a9e8ac66e24a0da79e79ade6f30bcf87e8
jekm <jekm@kn-tr-jekm-0055.efif.dk>
2014-04-03 13:26:05 +0200

    Personklasse tilføjet

---
+++ src/gittestproject2/Person.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gittestproject2;
+
+/**
+ *
+ * @author jekm
+ */
+public class Person {
+    String nsme, addresse;
+    public Person() {
+    }
+
+    public String getNsme() {
+        return nsme;
+    }
+
+    public void setNsme(String nsme) {
+        this.nsme = nsme;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
+    
+}
